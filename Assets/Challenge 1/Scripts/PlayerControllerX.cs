239cb413fccd68e6802288b285793819f86ab72a
Bertrand <mctchuente@gmail.com>
2022-08-22 08:55:05 +0100

    Plane actions implementation
    - Make the plane go forward
    - Slow the plane down to a manageable speed
    - Make the plane tilt only if the user presses the up/down arrows
    - Reposition the camera so it’s beside the plane
    - Make the camera follow the plane

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float verticalInput;
+    // Private Variables
+    private float speed = 15.0f;
+    private float rotationSpeed = 150.0f;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,11 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        //transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        //transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
